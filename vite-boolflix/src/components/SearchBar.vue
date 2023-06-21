9124363b09933375f81cd592444d066f5258823f
matteo-didone <matteo.dido9@gmail.com>
2023-06-21 18:13:07 +0200

    Added comments

--- vite-boolflix/src/components/SearchBar.vue
+++ vite-boolflix/src/components/SearchBar.vue
@@ -1,6 +1,10 @@
 <template>
     <nav class="navbar navbar-light bg-light">
         <div class="container-fluid">
+            <!-- 
+                v-model binds the input value to the data property searchQuery
+                @click calls the method searchMovies when the button is clicked
+            -->
             <input v-model="searchQuery" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button @click="searchMovies" class="btn btn-outline-success" type="button">Search</button>
         </div>
@@ -9,7 +13,12 @@
     <div class="movie-info">
         <h2>Movie Information:</h2>
         <ul>
+            <!-- 
+                v-for iterates over the movies array and displays the movie information
+                :key is used to give each movie a unique identifier
+            -->
             <li v-for="movie in movies" :key="movie.id">
+                <!-- Copied .title, .orginal_title, .original_language, .vote_average from the API code, visualized using Postman -->
                 <h3>{{ movie.title }}</h3>
                 <p>Original Title: {{ movie.original_title }}</p>
                 <p>Language: {{ movie.original_language }}</p>
@@ -37,12 +46,14 @@ export default {
 
         return {
             store,
+            // Adding searchQuery and movies to the data object
             searchQuery: '',
             movies: [],
         }
     },
 
     methods: {
+        // Declaring searchMovies method, inside which there's the API call
         searchMovies() {
             // Implemented API call to TMDB
             const options = {
@@ -56,6 +67,7 @@ export default {
             fetch(`https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&query=${this.searchQuery}`, options)
                 .then(response => response.json())
                 .then(data => {
+                    // We update the movie array with the results properties of the data object
                     this.movies = data.results;
                 })
                 .catch(err => console.error(err));
